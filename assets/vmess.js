b6a224d87a63e8869a66c032a34247651c7b4573
dongpo.li <dongpo.li@hotmail.com>
2024-03-16 17:01:17 +0800

    添加机场订阅地址

---
+++ assets/vmess.js
@@ -0,0 +1,56 @@
+function toBinaryStr(str) {
+    const encoder = new TextEncoder();
+    // 1: split the UTF-16 string into an array of bytes
+    const charCodes = encoder.encode(str);
+    // 2: concatenate byte data to create a binary string
+    return String.fromCharCode(...charCodes);
+}
+
+let tpl = {
+    "add": "4.00888.xyz",
+    "aid": "64",
+    "alpn": "",
+    "fp": "chrome",
+    "host": "4.00888.xyz",
+    "id": "ce57d713-c4f6-44bb-a59d-737a6080bb93",
+    "net": "ws",
+    "path": "/ZIq0394bkpr7Dj82",
+    "port": "443",
+    "ps": "美国-3",
+    "scy": "auto",
+    "sni": "4.00888.xyz",
+    "tls": "tls",
+    "type": "",
+    "v": "2"
+};
+
+let responseBody = '';
+
+
+let servers = {
+    "0": "美国-洛杉矶-1",
+    "1": "法国-斯特拉斯堡-1",
+    "2": "美国-洛杉矶-2",
+    "4": "美国-洛杉矶-3"
+};
+let domain = '00888.xyz';
+
+let protocol = "vmess://";
+
+for (const key in servers) {
+    if (Object.hasOwnProperty.call(servers, key)) {
+        const element = servers[key];
+
+        let host = key + '.' + domain;
+
+        tpl.add = host;
+        tpl.host = host;
+        tpl.sni = host;
+        tpl.ps = servers[key];
+
+        responseBody += protocol + btoa(toBinaryStr(JSON.stringify(tpl))) + '\n';
+    }
+}
+
+
+console.log(responseBody)
