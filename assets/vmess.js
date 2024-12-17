bfe20dca92ec53ab5e593f1032976bd875f05ae5
dongpo.li <dongpo.li@hotmail.com>
2024-12-17 17:03:03 +0800

    主和备份文件

--- assets/vmess.js
+++ assets/vmess.js
@@ -102,6 +102,47 @@ const vmess_tcp_tls_tpl = {
     "path": ""
 }
 let vmess_tcp_tls_servers = [
+    // 0
+    {
+        "domain": "888888888.uk",
+        "host": "0",
+        "name": "规则",
+        "uid": "7778bc68-9df0-4891-84c5-696de94c8531"
+    },
+    {
+        "domain": "888888888.uk",
+        "host": "0",
+        "name": "直连",
+        "uid": "ce57d713-c4f6-44bb-a59d-737a6080bb93"
+    },
+    {
+        "domain": "888888888.uk",
+        "host": "0",
+        "name": "亚利桑那-梅萨",
+        "uid": "b63e5bf6-cfc9-4b47-a07d-cd8be0f63a70"
+    },
+    {
+        "domain": "888888888.uk",
+        "host": "0",
+        "name": "香港",
+        "uid": "1a92a59b-e345-43c5-b822-87bb4ac7b51c"
+    },
+
+    // 3
+    {
+        "domain": "888888888.uk",
+        "host": "3",
+        "name": "洛杉矶-BWH-将过期-直连",
+        "uid": "ce57d713-c4f6-44bb-a59d-737a6080bb93"
+    },
+
+    // 4
+    {
+        "domain": "888888888.uk",
+        "host": "4",
+        "name": "大阪-BWH-直连"
+    },
+
     // 5
     {
         "domain": "888888888.uk",
@@ -120,47 +161,6 @@ let vmess_tcp_tls_servers = [
         "host": "5",
         "name": "洛杉矶-DMIT-主-落地(香港)",
         "uid": "1a92a59b-e345-43c5-b822-87bb4ac7b51c"
-    },
-
-    // 0
-    {
-        "domain": "888888888.uk",
-        "host": "0",
-        "name": "洛杉矶-BWH-备-规则",
-        "uid": "7778bc68-9df0-4891-84c5-696de94c8531"
-    },
-    {
-        "domain": "888888888.uk",
-        "host": "0",
-        "name": "洛杉矶-BWH-备-直连",
-        "uid": "ce57d713-c4f6-44bb-a59d-737a6080bb93"
-    },
-    {
-        "domain": "888888888.uk",
-        "host": "0",
-        "name": "洛杉矶-BWH-备-落地(亚利桑那-梅萨)",
-        "uid": "b63e5bf6-cfc9-4b47-a07d-cd8be0f63a70"
-    },
-    {
-        "domain": "888888888.uk",
-        "host": "0",
-        "name": "洛杉矶-BWH-备-落地(香港)",
-        "uid": "1a92a59b-e345-43c5-b822-87bb4ac7b51c"
-    },
-
-    // 3
-    {
-        "domain": "888888888.uk",
-        "host": "3",
-        "name": "洛杉矶-BWH-将过期-直连",
-        "uid": "ce57d713-c4f6-44bb-a59d-737a6080bb93"
-    },
-
-    // 4
-    {
-        "domain": "888888888.uk",
-        "host": "4",
-        "name": "大阪-BWH-直连"
     },
 
     // 6
@@ -197,6 +197,9 @@ for (let i = 0; i < vmess_tcp_tls_servers.length; i++) {
     // console.log(JSON.stringify(obj))
 
     console.log(protocol + btoa(toBinaryStr(JSON.stringify(obj))));
+    if (i == 3) {
+        console.log('\n\n\n\n\n');
+    }
 
     // responseBody += protocol + btoa(toBinaryStr(JSON.stringify(obj))) + '\n';
 }
